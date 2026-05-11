dd586f16c3355112d0238c2fbd23d069ca66677e
convertixhq <hello.convertix@gmail.com>
2026-05-11 18:40:07 +0100

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,5 +1,27 @@
 document.querySelectorAll("button").forEach(button => {
-  button.addEventListener("click", () => {
-    alert("Next step: booking form (coming next)");
-  });
+
+button.addEventListener("click", () => {
+
+const text = button.innerText.toLowerCase();
+
+if(text.includes("free")){
+
+window.location.href = "free.html";
+
+}
+
+else if(text.includes("priority")){
+
+window.location.href = "priority.html";
+
+}
+
+else if(text.includes("book")){
+
+window.location.href = "booking.html";
+
+}
+
+});
+
 });
