f45c755c31f17389671d890432dc6339bec574e5
convertixhq <hello.convertix@gmail.com>
2026-05-14 18:32:28 +0100

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,23 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if(target){
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('.top-nav');
+
+    if(window.scrollY > 40){
+        nav.classList.add('nav-scrolled');
+    } else {
+        nav.classList.remove('nav-scrolled');
+    }
+});
